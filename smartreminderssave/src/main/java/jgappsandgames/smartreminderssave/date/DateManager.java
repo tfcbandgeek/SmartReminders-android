95c22bc5e885c37bba858ce85d07c9da03d35ad7
Joshua Garner <josh.garner94@gmail.com>
2017-10-21 11:42:07 -0400

    - added day count

--- smartreminderssave/src/main/java/jgappsandgames/smartreminderssave/date/DateManager.java
+++ smartreminderssave/src/main/java/jgappsandgames/smartreminderssave/date/DateManager.java
@@ -92,6 +92,9 @@ public class DateManager {
     }
 
     // Getters
+    public static int getDayCount() {
+        return getWeekCount() * 7;
+    }
     public static int getWeekCount() {
         return weeks.size();
     }
