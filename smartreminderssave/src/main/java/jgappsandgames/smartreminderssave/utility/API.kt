588c4c55919c42a803eff565d894bfff6c877ee6
Joshua Garner <josh.garner94@gmail.com>
2017-10-30 19:39:11 -0400

    Building up new Classes

---
+++ smartreminderssave/src/main/java/jgappsandgames/smartreminderssave/utility/API.kt
@@ -0,0 +1,24 @@
+package jgappsandgames.smartreminderssave.utility
+
+/**
+ * API
+ * Created by joshua on 10/30/17.
+ *
+ * Holds the API Information (Changes, Data)
+ */
+
+// Release (10/3/17)
+val RELEASE = 10
+val RELEASE_INFO = "List<> for JSONArray access Points"
+
+// Management (12/1/17)
+val MANAGEMENT = 11
+val MANAGEMENT_INFO = "Managers Save, Universal Manager, Task Metadata, File Metadata, Change Listeners"
+
+// Attachments (3/1/17)
+val ATTACHMENTS = 12
+val ATTACHMENTS_INFO = "Task/Folder Attachments, Notes"
+
+// Folders (6/1/17)
+val FOLDERS = 13
+val FOLDERS_INFO = "Implement Folder as Folder"
