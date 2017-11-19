952f6f0acd3169d1d87232342b16e8e2a32182e1
Joshua Garner <josh.garner94@gmail.com>
2017-11-18 16:46:28 -0800

    Updated API Plan

--- smartreminderssave/src/main/java/jgappsandgames/smartreminderssave/utility/API.java
+++ smartreminderssave/src/main/java/jgappsandgames/smartreminderssave/utility/API.java
@@ -14,4 +14,8 @@ public class API {
     // Management (12/1/17*)
     public static final int MANAGEMENT = 11;
     public static final String MANAGEMENT_INFO = "ThemeManager, Universal Manager, Task Metadata, File Metadata";
+
+    // Shrinking (5/1/18*)
+    public static final int SHRINKING = 12;
+    public static final String SHRINKING_INFO = "Single Character Keys for Save File, Attachments, Open File Notices";
 }
