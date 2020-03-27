5162e2be8d4b4e10dc922854763f8e64bfc2b4a5
Joshua Garner <josh.garner94@gmail.com>
2020-03-27 14:06:58 -0700

    - A lot of random small things

--- smartreminderssave/src/main/java/jgappsandgames/smartreminderssave/utility/VersionUtility.kt
+++ smartreminderssave/src/main/java/jgappsandgames/smartreminderssave/utility/VersionUtility.kt
@@ -2,10 +2,15 @@ package jgappsandgames.smartreminderssave.utility
 
 import android.os.Build
 
-fun hasKitKat(): Boolean = Build.VERSION_CODES.KITKAT < Build.VERSION.SDK_INT
+fun hasKitKat(): Boolean = true
 fun hasShortcuts(): Boolean = Build.VERSION_CODES.N_MR1 < Build.VERSION.SDK_INT
 fun hasOreo(): Boolean = Build.VERSION_CODES.O < Build.VERSION.SDK_INT
+fun hasPie(): Boolean = Build.VERSION_CODES.P < Build.VERSION.SDK_INT
+fun hasQ(): Boolean = Build.VERSION_CODES.O < Build.VERSION.SDK_INT
 
+fun getLineSeparator(): String = System.lineSeparator()
+
+@Deprecated("Mispelled")
 fun getLineSeperator(): String {
     return if (hasKitKat()) System.lineSeparator()
     else System.getProperty("line.separator")!!
